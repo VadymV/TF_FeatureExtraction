d9b6cc889ec246de6528f9131bb89a92a1b384fb
Vadym Gryshchuk <2gryshch@informatik.uni-hamburg.de>
2018-08-15 19:24:09 +0200

    Modifications: extract features

--- example_feat_extract.py
+++ example_feat_extract.py
@@ -1,6 +1,8 @@
 # MIT License
 #
 # Copyright (c) 2017 Tom Runia
+# Modifications copyright (C) 2018  Vadym Gryshchuk (vadym.gryshchuk@protonmail.com)
+# Date modified: 29 July 2018
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -8,6 +10,12 @@
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to conditions.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+# OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 #
 # Author: Tom Runia
 # Date Created: 2017-08-15
@@ -17,12 +25,32 @@ from __future__ import division
 from __future__ import print_function
 
 import argparse
+
+import h5py
 import numpy as np
 import time
 from datetime import datetime
 
 from feature_extractor.feature_extractor import FeatureExtractor
 import feature_extractor.utils as utils
+
+CATEGORIES_ICWT = {'book': 0, 'cellphone': 1, 'mouse': 2, 'pencilcase': 3, 'ringbinder': 4,
+              'hairbrush': 5, 'hairclip': 6, 'perfume': 7, 'sunglasses': 8, 'wallet': 9,
+              'flower': 10, 'glass': 11, 'mug': 12, 'remote': 13, 'soapdispenser': 14,
+              'bodylotion': 15, 'ovenglove': 16, 'sodabottle': 17, 'sprayer': 18, 'squeezer': 19}
+
+SESSIONS_ICWT = {'MIX':1, 'ROT2D':2, 'ROT3D':3, 'SCALE':4, 'TRANSL':5}
+
+
+DAYS_ICWT = {'day1': 1, 'day2': 2, 'day3': 3, 'day4': 4, 'day5': 5, 'day6': 6, 'day7': 7, 'day8': 8, 'day9': 9, 'day10': 10,
+        'day11': 11, 'day12': 12, 'day13': 13, 'day14': 14, 'day15': 15}
+
+CAMERAS_ICWT = {'left': 1, 'right': 2}
+
+CORE50_ADDITIONAL_COLUMNS = 4
+ICWT_ADDITIONAL_COLUMNS = 6
+NICO_ADDITIONAL_COLUMNS = 4
+
 
 
 def feature_extraction_queue(feature_extractor, image_path, layer_names,
@@ -122,12 +150,19 @@ if __name__ == "__main__":
     parser.add_argument("--network", dest="network_name", type=str, required=True, help="model name, e.g. 'resnet_v2_101'")
     parser.add_argument("--checkpoint", dest="checkpoint", type=str, required=True, help="path to pre-trained checkpoint file")
     parser.add_argument("--image_path", dest="image_path", type=str, required=True, help="path to directory containing images")
-    parser.add_argument("--out_file", dest="out_file", type=str, default="./features.h5", help="path to save features (HDF5 file)")
+    # Start: Modifications (Vadym Gryshchuk).
+    parser.add_argument("--out_features_h5", dest="out_file", type=str, default="./features.h5", help="path to save features (HDF5 file)")
+    parser.add_argument("--out_features_csv", dest="out_features_file", type=str, default="./features.csv", help="path to save features (CSV file)")
+    # End: Modifications (Vadym Gryshchuk).
+
     parser.add_argument("--layer_names", dest="layer_names", type=str, required=True, help="layer names separated by commas")
     parser.add_argument("--preproc_func", dest="preproc_func", type=str, default=None, help="force the image preprocessing function (None)")
     parser.add_argument("--preproc_threads", dest="num_preproc_threads", type=int, default=2, help="number of preprocessing threads (2)")
     parser.add_argument("--batch_size", dest="batch_size", type=int, default=64, help="batch size (32)")
-    parser.add_argument("--num_classes", dest="num_classes", type=int, default=1001, help="number of classes (1001)")
+    parser.add_argument("--num_classes", dest="num_classes", type=int, default=1001, help="number of classes for the CNN model (1001)")
+    # Start: Modifications (Vadym Gryshchuk).
+    parser.add_argument("--dataset_name", dest="dataset_name", type=str, default="nico", help="'iCubWorld' or 'CORe50'")
+    # End: Modifications (Vadym Gryshchuk).
     args = parser.parse_args()
 
     # resnet_v2_101/logits,resnet_v2_101/pool4 => to list of layer names
@@ -151,6 +186,116 @@ if __name__ == "__main__":
         feature_extractor, args.image_path, layer_names,
         args.batch_size, args.num_classes)
 
+    # Modifications (Vadym Gryshchuk). Start.
+    features_temp = feature_dataset.get(layer_names[0])
+    if features_temp.shape.__len__() == 4:
+        features_temp1 = np.squeeze(features_temp, axis=2)
+        features = np.squeeze(features_temp1, axis=1)
+    else:
+        features = feature_dataset.get(layer_names[0])
+
+    labels = feature_dataset.get("filenames")
+
+    if args.dataset_name == 'CORe50':
+        features_array = np.zeros((features.shape[0], features.shape[1] + CORE50_ADDITIONAL_COLUMNS), dtype=np.float64)
+
+        # Find the appropriate ids for each object.
+        for index_label in range(len(labels)):
+
+            # Get the current label
+            label = labels[index_label]
+
+            # Get the features for this label
+            temp = features[index_label]
+
+            # Get object's id.
+            object_id = int(str(label).split('_')[2]) - 1
+
+            # Get session's id.
+            session = int(str(label).split('_')[1])
+
+            # Get image's id.
+            image_id = int(str(label).split('_')[3].split('.')[0])
+
+            if 0 <= object_id <= 4:
+                category = 0
+            if 5 <= object_id <= 9:
+                category = 1
+            if 10 <= object_id <= 14:
+                category = 2
+            if 15 <= object_id <= 19:
+                category = 3
+            if 20 <= object_id <= 24:
+                category = 4
+            if 25 <= object_id <= 29:
+                category = 5
+            if 30 <= object_id <= 34:
+                category = 6
+            if 35 <= object_id <= 39:
+                category = 7
+            if 40 <= object_id <= 44:
+                category = 8
+            if 45 <= object_id <= 49:
+                category = 9
+
+            temp1 = np.append(temp, image_id)
+            temp2 = np.append(temp1, session)
+            temp3 = np.append(temp2, category)
+            temp4 = np.append(temp3, object_id)
+
+            features_array[index_label] = temp4
+
+        feature_dataset[layer_names[0]] = features_array
+
+    if args.dataset_name == 'ICWT':
+
+        features_array = np.zeros((features.shape[0], features.shape[1] + ICWT_ADDITIONAL_COLUMNS), dtype=np.float64)
+
+        # Find the appropriate ids for each object.
+        for index_label in range(len(labels)):
+
+            # Get the current label
+            label = labels[index_label].decode("utf-8").split('_')
+
+            # Get the features for this label
+            temp = features[index_label]
+
+            temp1 = np.append(temp, CATEGORIES_ICWT.get(label[1]))
+            temp2 = np.append(temp1, label[2])
+            temp3 = np.append(temp2, SESSIONS_ICWT.get(label[3]))
+            temp4 = np.append(temp3, DAYS_ICWT.get(label[4]))
+            temp5 = np.append(temp4, CAMERAS_ICWT.get(label[5]))
+            temp6 = np.append(temp5, label[6][:-4])
+
+            features_array[index_label] = temp6
+
+        feature_dataset[layer_names[0]] = features_array
+
+    if args.dataset_name == 'NICO':
+
+        # Extend the number of columns.
+        features_array = np.zeros((features.shape[0], features.shape[1] + NICO_ADDITIONAL_COLUMNS), dtype=np.float64)
+
+        # Find the object id, session id and category id for each object.
+        for index_label in range(len(labels)):
+
+            # Get the current label.
+            label1 = labels[index_label].decode("utf-8").split('/')
+            label2 = label1[len(label1) - 1]
+            label3 = label2.split('_')
+
+            # Get the features for this label.
+            temp = features[index_label]
+            temp0 = np.append(temp, int(label3[0]) - 1)   # category
+            temp1 = np.append(temp0, int(label3[1]) - 1)  # object class
+            temp2 = np.append(temp1, label3[2])  # session
+            temp3 = np.append(temp2, label3[3][:-4])  # image id within the object class
+            features_array[index_label] = temp3
+
+        feature_dataset[layer_names[0]] = features_array
+
+    # End: Modifications (Vadym Gryshchuk).
+
     # Write features to disk as HDF5 file
     utils.write_hdf5(args.out_file, layer_names, feature_dataset)
     print("Successfully written features to: {}".format(args.out_file))
@@ -158,3 +303,14 @@ if __name__ == "__main__":
     # Close the threads and close session.
     feature_extractor.close()
     print("Finished.")
+
+    # Start: Modifications (Vadym Gryshchuk).
+
+    f = h5py.File(args.out_file, 'r')
+    dataset = f[args.layer_names][...]
+
+    np.savetxt(args.out_features_file, dataset, delimiter=',')
+    np.save
+    f.close()
+
+    # End: Modifications (Vadym Gryshchuk).
